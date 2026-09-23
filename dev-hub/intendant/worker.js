b02e8402e1ef9efeae906940dee1bb8a90b7919b
chachasan090375 <chachasan090375@gmail.com>
2026-09-23 11:56:28 +0200

    feat(dev-hub): add intendant central authority worker

---
+++ dev-hub/intendant/worker.js
@@ -0,0 +1,2 @@
+import {makeAuthority} from "../specialists/core.js";
+export default makeAuthority({role:"intendant",scope:"COST_RESOURCES",blockSeverities:["CRITICAL"],reviseSeverities:["BLOCK"]});
